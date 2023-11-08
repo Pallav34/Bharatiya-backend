6d825c03ad0d819e316624b96bab6a4c85ad33eb
Pallav34 <pallavbag1@gmail.com>
2023-11-08 21:43:00 +0530

    cookie fixed

--- utils/features.js
+++ utils/features.js
@@ -3,6 +3,7 @@ exports.sendcookie = (user,res,message,statusCode=200) =>{
     const token = jwt.sign({_id:user._id},process.env.JWT_SECRET);
 
     res.status(statusCode).cookie("token",token,{
+
     }).json({
         success:true,
         message: message
