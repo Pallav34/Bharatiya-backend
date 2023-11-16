aff7cfd49e9efbd163135f6dbc9ef2045cc2a528
Pallav34 <pallavbag1@gmail.com>
2023-11-17 00:31:14 +0530

    maxage updated

--- utils/features.js
+++ utils/features.js
@@ -4,7 +4,7 @@ exports.sendcookie = (user,res,message,statusCode=200) =>{
 
     res.status(statusCode).cookie("token",token,{
         httpOnly: true,
-        maxAge: 1800000,  
+        maxAge: 30*60*60*1000,  
         sameSite: process.env.NODE_ENV === "Development" ? "lax" :"none",
         secure:process.env.NODE_ENV === "Development" ? false : true,
     }).json({
