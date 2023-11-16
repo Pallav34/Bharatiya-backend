9e1d2304c9e0313562d0b77bc6ce899c3ca5245b
Pallav34 <pallavbag1@gmail.com>
2023-11-16 19:08:10 +0530

    cookie fixed

--- utils/features.js
+++ utils/features.js
@@ -4,7 +4,7 @@ exports.sendcookie = (user,res,message,statusCode=200) =>{
 
     res.status(statusCode).cookie("token",token,{
         httpOnly: true,
-        maxAge: 30 *60 * 1000,  
+        maxAge: 15 *60 * 1000,  
         sameSite: process.env.NODE_ENV === "Development" ? "lax" :"none",
         secure:process.env.NODE_ENV === "Development" ? false : true,
     }).json({
